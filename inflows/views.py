6821d0930a708e57c80830641ac0b17f60191138
Leonardo <leo.sales.br@gmail.com>
2026-04-30 16:40:30 -0300

    finish apps

--- inflows/views.py
+++ inflows/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import models, forms
 
-# Create your views here.
+
+class InflowListView(ListView):
+    model = models.Inflow
+    template_name = "Inflow_list.html"
+    context_object_name = "Inflows"
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        product = self.request.GET.get("product")
+
+        if product:
+            queryset = queryset.filter(product__title__icontains=product)
+        return queryset
+
+class InflowCreateView(CreateView):
+    model = models.Inflow
+    template_name = "Inflow_create.html"
+    form_class = forms.InflowForm
+    success_url = reverse_lazy("Inflow_list")
+
+class InflowDetailView(DetailView):
+    model = models.Inflow
+    template_name = "Inflow_detail.html"
--- products/views.py
+++ products/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import models, forms
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = models.Product
+    template_name = "Product_list.html"
+    context_object_name = "Products"
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.GET.get("title")
+
+        if title:
+            queryset = queryset.filter(name__icontains=title)
+        return queryset
+
+class ProductCreateView(CreateView):
+    model = models.Product
+    template_name = "Product_create.html"
+    form_class = forms.ProductForm
+    success_url = reverse_lazy("Product_list")
+
+class ProductDetailView(DetailView):
+    model = models.Product
+    template_name = "Product_detail.html"
+
+class ProductUpdateView(UpdateView):
+    model = models.Product
+    template_name = "Product_update.html"
+    form_class = forms.ProductForm
+    success_url = reverse_lazy("Product_list")
+
+class ProductDeleteView(DeleteView):
+    model = models.Product
+    template_name = "Product_delete.html"
+    success_url = reverse_lazy("Product_list")
